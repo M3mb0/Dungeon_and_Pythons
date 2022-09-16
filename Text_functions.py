fe48b73c04b51f984bd74ccb6957fd72ce40eef2
M3mb0 <cristian.ungureanu.86@gmail.com>
2022-09-16 11:01:59 +0100

    creation of text functions

---
+++ Text_functions.py
@@ -0,0 +1,46 @@
+class TextFunction:
+
+    @staticmethod
+    def intro_message():
+        print("""Welcome stranger.
+          ->  Here in Hinderlands, you'll get
+          to fight strong opponents and
+          conquer the deadliest dungeons.
+          ->  In a country
+          where magic rules,
+          anything is possible if
+          you wish so.
+          ->  It all depends
+        """)
+
+
+    @staticmethod
+    def wizard_message():
+        print("""Congratulations,
+          grand wizard!
+          ->  Wizards were
+          the scholars of 
+          Hinderlands,
+          spreading knowledge
+          and hope amongst
+          the people.
+          ->  Are you going 
+          to help and nurture
+          those in need, or turn ? 
+        """)
+
+
+    @staticmethod
+    def warrior_message():
+        print("""Congratulations,
+          great warrior!
+          ->  Warrior were
+          high regarded in
+          Hinderlands,as they
+          were the only
+          protectors of common
+          folks.
+          ->  Are you going
+          to protect the
+          common people, ot turn? 
+        """)
