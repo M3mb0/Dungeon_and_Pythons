cdfec87431b02c7e096c6f20e13a2882bdd2ed7f
M3mb0 <cristian.ungureanu.86@gmail.com>
2022-09-19 16:51:22 +0100

    Added the methods in main(where the game is running)

--- main.py
+++ main.py
@@ -3,4 +3,10 @@ from Text_functions import *
 
 TextFunction.intro_message()
 user = input('Do you want to play? Press \'y\'for yes and \'n\' for exit the game\n')
-TextFunction.ask_if_play(user)
+if TextFunction.ask_if_play(user):
+    user = input("""What character do you want to play:
+                1. Wizard
+                2. Warrior
+                Please select 1 or 2.
+                """)
+    TextFunction.ask_type_of_char(user)
