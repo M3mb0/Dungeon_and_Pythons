433802a437aecd9d689938fd99b0a8faf50a43d4
M3mb0 <cristian.ungureanu.86@gmail.com>
2022-09-23 22:56:48 +0300

    The fallowing attributes added: type_of, critical. attack method updated using critical Each enemy has a certain critical value

--- Enemy.py
+++ Enemy.py
@@ -1,27 +1,40 @@
+import random
+
+
 class Enemy:
 
-    def __init__(self, hp, power, armor):
+    def __init__(self, type_of, hp, power, defence):
+        self.type_of = type_of
         self.hp = hp
         self.power = power
-        self.armor = armor
+        self.defence = defence
+        self.critical = None
 
     def attack(self, user):
-        user.hp -= self.power - user.armor
+        if random.randint(1, 100) <= self.critical:
+            user.hp -= (self.power - user.defence) * 2
+            user.defence -= 5
+        else:
+            user.hp -= self.power - user.defence
+            user.defence -= 5
 
 
 class Goblin(Enemy):
 
-    def __init__(self, hp=50, power=50, armor=10):
-        super().__init__(hp, power, armor)
+    def __init__(self, type_of='Goblin', hp=500, power=70, defence=10):
+        super().__init__(type_of, hp, power, defence)
+        self.critical = 10
 
 
 class Orc(Enemy):
 
-    def __init__(self, hp=70, power=70, armor=50):
-        super().__init__(hp, power, armor)
+    def __init__(self, type_of='Orc', hp=800, power=100, defence=50):
+        super().__init__(type_of, hp, power, defence)
+        self.critical = 20
 
 
 class Troll(Enemy):
 
-    def __init__(self, hp=100, power=100, armor=100):
-        super().__init__(hp, power, armor)
+    def __init__(self, type_of='Troll', hp=1200, power=200, defence=100):
+        super().__init__(type_of, hp, power, defence)
+        self.critical = 50
