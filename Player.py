1718caba2fdbe4d3219ae9cf2933db641503479e
M3mb0 <cristian.ungureanu.86@gmail.com>
2022-09-23 22:58:22 +0300

    weapon and armor attributes added. attack method updated gear_up and open chest methods created

--- Player.py
+++ Player.py
@@ -1,23 +1,36 @@
+import random
+
+
 class Player:
 
-    def __init__(self, name, hp, power, armor):
+    def __init__(self, name, hp, power, defence, weapon, armor):
         self.hp = hp
         self.name = name
         self.power = power
+        self.defence = defence
+        self.weapon = weapon
         self.armor = armor
-        self.inventory = []
 
     def attack(self, user):
-        user.hp -= self.power - user.armor
+        user.hp -= self.power - user.defence
+        user.defence -= 5
+
+    def gear_up(self):
+        self.power += self.weapon
+        self.defence += self.armor
+
+    def open_chest(self):
+        self.weapon = random.randrange(100, 500, 50)
+        self.armor = random.randrange(10, 100, 10)
 
 
 class Wizard(Player):
 
-    def __init__(self, name, hp=50, power=120, armor=10):
-        super().__init__(name, hp, power, armor)
+    def __init__(self, name, hp=500, power=120, defence=30, weapon=100, armor=20):
+        super().__init__(name, hp, power, defence, weapon, armor)
 
 
 class Warrior(Player):
 
-    def __init__(self, name, hp=120, power=50, armor=50):
-        super().__init__(name, hp, power, armor)
+    def __init__(self, name, hp=1200, power=50, defence=50, weapon=100, armor=20):
+        super().__init__(name, hp, power, defence, weapon, armor)
