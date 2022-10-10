88251eb6938ce2a3d30deddee80be9d8aeeea64f
M3mb0 <cristian.ungureanu.86@gmail.com>
2022-10-10 20:35:04 +0300

    Adding json converter methods

--- useful_function.py
+++ useful_function.py
@@ -1,6 +1,19 @@
 import os
 import sys
 import time
+import json
+
+
+def retrive_sample_data():
+    file = open('players.json', 'r')
+    result = json.load(file)
+    file.close()
+    return result
+
+
+def dump_dict_to_json(my_data):
+    with open("players.json", "a") as fp:
+        json.dump(my_data, fp, indent=4)
 
 
 def my_print(text):
